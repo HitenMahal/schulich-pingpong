47c5bc99b005a0b7163e68f00275fa872987dfa2
JasonPang86 <jason.p.86.ar12@gmail.com>
2022-12-04 17:29:13 -0700

    Combining SQL statements with functionality class Update

--- SQLDriver.py
+++ SQLDriver.py
@@ -27,3 +27,27 @@ def add_new_profile(UCID, Username, Email, Type):
     cursor = db.cursor()
     cursor.execute(f"INSERT INTO ENDUSER (UCID, name, google_email, user_type) VALUES ( {UCID} , {Username}, {Email}, {Type} )")
 
+def delete_profile(UCID):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM ENDUSER WHERE UCID = {UCID}")
+
+def get_user_profile(UCID):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM ENDUSER WHERE UCID = {UCID}")
+
+def get_user_stats(UCID):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM STATS WHERE UCID = {UCID}")
+
+def add_stats(UCID, MatchesWon, MatchesPlayed, HoursPlayed):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO STATS VALUES (UCID, stat_distinguisher, {MatchesPlayed}, {MatchesWon}, {HoursPlayed})")
+
+def delete_stats(UCID):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO STATS VALUES (UCID, stat_distinguisher, {MatchesPlayed}, {MatchesWon}, {HoursPlayed})")
