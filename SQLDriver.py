dd4e53ee8f1afb494ae8247382dda84cd5931ff2
JasonPang86 <jason.p.86.ar12@gmail.com>
2022-12-04 20:21:30 -0700

    SQLDriver class fixes, adding in f before execute and adding in commit before close

--- SQLDriver.py
+++ SQLDriver.py
@@ -107,216 +107,247 @@ def add_new_profile(UCID, Password, Name, Email):
 def delete_profile(UCID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM ENDUSER WHERE UCID = {UCID}")
+    cursor.execute(f"DELETE FROM ENDUSER WHERE UCID = {UCID}")
     cursor.close()
 
 def edit_profile(ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("UPDATE ENDUSER SET UCID=UCID_value, name=name_value, google_email=google_email_value, user_type=user_type_value WHERE UCID= {ucid}")
+    cursor.execute(f"UPDATE ENDUSER SET UCID=UCID_value, name=name_value, google_email=google_email_value, user_type=user_type_value WHERE UCID= {ucid}")
+    db.commit()
     cursor.close()
 
 def get_user_profile(UCID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM ENDUSER WHERE UCID = {UCID}")
+    cursor.execute(f"SELECT * FROM ENDUSER WHERE UCID = {UCID}")
     cursor.close()
 
 def get_user_stats(UCID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM STATS WHERE UCID = {UCID}")
+    cursor.execute(f"SELECT * FROM STATS WHERE UCID = {UCID}")
     cursor.close()
 
 def add_stats(UCID, MatchesWon, MatchesPlayed, HoursPlayed):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO STATS VALUES (UCID, stat_distinguisher, {MatchesPlayed}, {MatchesWon}, {HoursPlayed})")
+    cursor.execute(f"INSERT INTO STATS VALUES (UCID, stat_distinguisher, {MatchesPlayed}, {MatchesWon}, {HoursPlayed})")
+    db.commit()
     cursor.close()
 
 def delete_stats(UCID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM STATS WHERE UCID = {UCID}")
+    cursor.execute(f"DELETE FROM STATS WHERE UCID = {UCID}")
+    db.commit()
     cursor.close()
 
 def edit_stats(ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("UPDATE STATS SET stat_distinguisher = stat_distinguisher_value, matches_played = matches_played_value, matches_won = matches_won_value, hours_played = hours_played_value WHERE UCID = {UCID}")
+    cursor.execute(f"UPDATE STATS SET stat_distinguisher = stat_distinguisher_value, matches_played = matches_played_value, matches_won = matches_won_value, hours_played = hours_played_value WHERE UCID = {ucid}")
+    db.commit()
     cursor.close()
 
 
 def new_team(team_ID, team_name, team_type):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO TEAM VALUES ({team_ID}, L_name_value, {team_name}, {team_type}")
+    cursor.execute(f"INSERT INTO TEAM VALUES ({team_ID}, L_name_value, {team_name}, {team_type}")
+    db.commit()
     cursor.close()
 
 def delete_team(team_ID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM TEAM WHERE team_id = {team_ID}")
+    cursor.execute(f"DELETE FROM TEAM WHERE team_id = {team_ID}")
+    db.commit()
     cursor.close()
 
 def edit_team(team_ID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("UPDATE TEAM SET L_name = L_name_value, team_name = team_name_value, team_type = team_type_value WHERE team_id = {team_ID}")
+    cursor.execute(f"UPDATE TEAM SET L_name = L_name_value, team_name = team_name_value, team_type = team_type_value WHERE team_id = {team_ID}")
+    db.commit()
     cursor.close()
 
 def get_teamMember_stats(team_ID):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("SELECT s.UCID, s.stat_distinguisher, s.matches_played, s.matches_won, s.hours_player FROM STATS as s NATURAL JOIN (SELECT UCID FROM USER_IN_TEAM WHERE team_id  = {team_ID})")
+    cursor.execute(f"SELECT s.UCID, s.stat_distinguisher, s.matches_played, s.matches_won, s.hours_player FROM STATS as s NATURAL JOIN (SELECT UCID FROM USER_IN_TEAM WHERE team_id  = {team_ID})")
     cursor.close()
 
 def add_team_member(ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO TEAM (UCID) VALUES (UCID = {ucid})")
+    cursor.execute(f"INSERT INTO TEAM (UCID) VALUES (UCID = {ucid})")
+    db.commit()
     cursor.close()
 
 def remove_team_member(ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM TEAM WHERE UCID = {ucid}")
+    cursor.execute(f"DELETE FROM TEAM WHERE UCID = {ucid}")
+    db.commit()
     cursor.close()
 
 def get_all_teams_with_user(ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("SELECT team_id FROM TEAM WHERE UCID = {ucid}")
+    cursor.execute(f"SELECT team_id FROM TEAM WHERE UCID = {ucid}")
     cursor.close()
 
 def new_booking(schedule_num, ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO BOOKING (SSchedule#, UCID) VALUES ( SSchedule# = {schedule}, UCID = {ucid})")
+    cursor.execute(f"INSERT INTO BOOKING (SSchedule#, UCID) VALUES ( SSchedule# = {schedule_num}, UCID = {ucid})")
+    db.commit()
     cursor.close()
 
 def delete_booking(time_slot):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM BOOKING WHERE for_timestamp = {time_slot}")
+    cursor.execute(f"DELETE FROM BOOKING WHERE for_timestamp = {time_slot}")
+    db.commit()
     cursor.close()
 
 def new_rental(ucid, rental_id, start_time, return_time, deposit):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO RENTAL VALUES ({ucid}, {rental_id}, {start_time}, {return_time}, {deposit})")
+    cursor.execute(f"INSERT INTO RENTAL VALUES ({ucid}, {rental_id}, {start_time}, {return_time}, {deposit})")
+    db.commit()
     cursor.close()
 
 def edit_rental(rental_id):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("UPDATE RENTAL SET L_name = UCID = UCID_value, rental_id = rental_id_value, start_time = start_time_value, return_time = return_time_value, deposit = deposit_value WHERE rental_id= {rental_id}")
+    cursor.execute(f"UPDATE RENTAL SET L_name = UCID = UCID_value, rental_id = rental_id_value, start_time = start_time_value, return_time = return_time_value, deposit = deposit_value WHERE rental_id= {rental_id}")
+    db.commit()
     cursor.close()
 
 def new_match(match_id, ucid, score, date):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO MATCH VALUES ({match_id}, {ucid}, {score}, {date})")
+    cursor.execute(f"INSERT INTO MATCH VALUES ({match_id}, {ucid}, {score}, {date})")
+    db.commit()
     cursor.close()
 
 def cancel_match(match_id):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM MATCH WHERE match_id = {match_id}")
+    cursor.execute(f"DELETE FROM MATCH WHERE match_id = {match_id}")
+    db.commit()
     cursor.close()
 
 def new_leaderboard(name, event_name, building_name):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO LEADERBOARD (Name, E_name, B_name) VALUES (Name = {name}, E_name = {event_name}, B_name = {building_name})")
+    cursor.execute(f"INSERT INTO LEADERBOARD (Name, E_name, B_name) VALUES (Name = {name}, E_name = {event_name}, B_name = {building_name})")
+    db.commit()
     cursor.close()
 
 def delete_leaderboard(name):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM LEADERBOARD WHERE name = {name}")
+    cursor.execute(f"DELETE FROM LEADERBOARD WHERE name = {name}")
+    db.commit()
     cursor.close()
 
 def new_schedule(table_num, schedule_num):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO SCHEDULE VALUES ({table_num}, {schedule_num})")
+    cursor.execute(f"INSERT INTO SCHEDULE VALUES ({table_num}, {schedule_num})")
+    db.commit()
     cursor.close()
 
 def delete_schedule(table_num, schedule_num):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM SCHEDULE WHERE table_num = {table_num}, schedule_num = {schedule_num}")
+    cursor.execute(f"DELETE FROM SCHEDULE WHERE table_num = {table_num}, schedule_num = {schedule_num}")
+    db.commit()
     cursor.close()
 
 def new_equipment(type, max_rental_time, building_name):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO EQUIPMENT VALUES ({type}, {max_rental_time}, {building_name})")
+    cursor.execute(f"INSERT INTO EQUIPMENT VALUES ({type}, {max_rental_time}, {building_name})")
+    db.commit()
     cursor.close()
 
 def delete_equipment(equipment_id):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM EQUIPMENT WHERE equipment_id = {equipment_id}")
+    cursor.execute(f"DELETE FROM EQUIPMENT WHERE equipment_id = {equipment_id}")
+    db.commit()
     cursor.close()
 
 def add_player_ID(match_id, player_ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO MATCH_PLAYER_IDS VALUES ({match_id}, {player_ucid})")
+    cursor.execute(f"INSERT INTO MATCH_PLAYER_IDS VALUES ({match_id}, {player_ucid})")
+    db.commit()
     cursor.close()
 
 def remove_player_ID(player_id, player_ucid):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM MATCH_PLAYER_IDS WHERE player_id = {player_id}, p_ucid = {player_ucid}")
+    cursor.execute(f"DELETE FROM MATCH_PLAYER_IDS WHERE player_id = {player_id}, p_ucid = {player_ucid}")
+    db.commit()
     cursor.close()
 
 def add_event(building_name, event_name):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO EVENTS VALUES ({building_name}, {event_name})")
+    cursor.execute(f"INSERT INTO EVENTS VALUES ({building_name}, {event_name})")
+    db.commit()
     cursor.close()
 
 def remove_event(building_name, event_name):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM EVENTS WHERE B_name = {building_name}, E_name = {event_name}")
+    cursor.execute(f"DELETE FROM EVENTS WHERE B_name = {building_name}, E_name = {event_name}")
+    db.commit()
     cursor.close()
 
 def new_building(name, location, fac):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO BUILDING (Name, Location,Facilities) VALUES (Name = {name}, Location = {location},Facilities = {fac})")
+    cursor.execute(f"INSERT INTO BUILDING (Name, Location,Facilities) VALUES (Name = {name}, Location = {location},Facilities = {fac})")
+    db.commit()
     cursor.close()
 
 def delete_building(name):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM BUILDING WHERE Name = {name}")
+    cursor.execute(f"DELETE FROM BUILDING WHERE Name = {name}")
+    db.commit()
     cursor.close()   
 
 def add_time_slot(schedule_num, time_slot): 
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO SCHEDULE_TIME_SLOTS VALUES ({schedule_num}, {time_slot})")
+    cursor.execute(f"INSERT INTO SCHEDULE_TIME_SLOTS VALUES ({schedule_num}, {time_slot})")
+    db.commit()
     cursor.close()   
 
 def remove_time_slot(schedule_num, time_slot):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM SCHEDULE_TIME_SLOTS WHERE schedule_num = {schedule_num}, time_slot = {time_slot}")
+    cursor.execute(f"DELETE FROM SCHEDULE_TIME_SLOTS WHERE schedule_num = {schedule_num}, time_slot = {time_slot}")
+    db.commit()
     cursor.close()  
 
 def add_table(building_name, table_num):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO TABLE VALUES ({building_name}, {table_num})")
+    cursor.execute(f"INSERT INTO TABLE VALUES ({building_name}, {table_num})")
+    db.commit()
     cursor.close()  
 
 def remove_table(building_name, table_num):
     db = connect_db()
     cursor = db.cursor()
-    cursor.execute("DELETE FROM TABLE WHERE b_name= {building_name}, table_num= {table_num}")
+    cursor.execute(f"DELETE FROM TABLE WHERE b_name= {building_name}, table_num= {table_num}")
+    db.commit()
     cursor.close()  
